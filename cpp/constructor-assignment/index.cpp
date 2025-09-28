242be16c40710db72211359a67cbf77fe75e85e0
Kenneth Carpo <kennethcarpo169@gmail.com>
2025-09-28 18:23:11 +0800

    ,

--- cpp/constructor-assignment/index.cpp
+++ cpp/constructor-assignment/index.cpp
@@ -24,6 +24,9 @@ class bankAccount{
     cout << "PIN                : " << pin << endl;
     cout << "===============================\n" << endl ;
 }
+    void displayBal() {
+    cout << "Hello! " << accountName << " your remaining balance is PHP"<< balance << endl;
+    }
 };
 
 
@@ -32,14 +35,74 @@ int main () {
 bankAccount account1("Maria Santos",    100001, 2500.75, 1234);
 bankAccount account2("Juan Dela Cruz",  100002, 5300.50, 5678);
 bankAccount account3("Andrea Lopez",    100003, 15000.00, 4321);
-bankAccount account4("Michael Tan",     100004, 890.25, 2468);
-bankAccount account5("Sophia Reyes",    100005, 120000.99, 1357);
 
-account1.displayAll();
-account2.displayAll();
-account3.displayAll();
-account4.displayAll();
-account5.displayAll();
+
+int choice; // 1.View account 2. Check balance 3. View All accounts  4. Exit
+char accChoice;
+
+cout << "\n===== MENU =====" << endl;
+cout << "1. View account" << endl;
+cout << "2. Check balance" << endl;
+cout << "3. View all accounts" << endl;
+cout << "4. Exit" << endl;
+cout << "Enter your choice: ";
+cin >> choice;
+
+switch (choice) {
+    case 1: // view 1 acc
+        cout << "\n***** Choose your Account Name *****\n" << endl;
+        cout << "a. " << account1.accountName << endl;
+        cout << "b. " << account2.accountName << endl;
+        cout << "c. " << account3.accountName << endl;
+        cout << "Enter your choice: ";
+        cin >> accChoice;
+
+        switch(accChoice) {
+            case 'a':
+                account1.displayAll();
+                break;
+            case 'b':
+                account2.displayAll();
+                break;
+            case 'c':
+                account3.displayAll();
+                break;
+            default:
+                cout << "Account not found.\n";
+                break;
+        }
+    break;
+    case 2: // view bal
+        cout << "***** Choose your Account Name *****" << endl;
+        cout << "a. " << account1.accountName << endl;
+        cout << "b. " << account2.accountName << endl;
+        cout << "c. " << account3.accountName << endl;
+        cout << "Enter your choice: ";
+        cin >> accChoice;
+
+        if (accChoice == 'a') {
+            account1.displayBal();
+        }
+        else if (accChoice == 'b') {
+            account2.displayBal();
+        }
+        else if (accChoice == 'c') {
+            account3.displayBal();
+        }
+        else {
+            cout << "Account not found";
+        }
+    break;
+    case 3: // view all
+        account1.displayAll();
+        account2.displayAll();
+        account3.displayAll();
+    break;
+    case 4: // exit
+    break;
+
+        
+}
 
 }
 
---
+++ cpp/constructor-assignment/tempCodeRunnerFile.cpp
@@ -0,0 +1,22 @@
+int choice; // 1.View account 2. Check balance 3. View All accounts  4. Exit
+int accChoice;
+
+
+
+
+
+switch (choice) {
+    case 1: // view 1 acc
+        case 'a':
+            account1.displayAll();
+        case 'b':
+            account2.displayAll();
+        case 'c':
+            account3.displayAll();
+    break;
+    case 2: // view bal
+
+    case 3: // view all
+
+    case 4: // exit
+}
