7f15b718bbdc8e8f883e7606594f8e277e688041
Kenneth Carpo <kennethcarpo169@gmail.com>
2025-10-03 00:56:21 +0800

    ayoko na

--- cpp/HotelReservationSystem/main.cpp
+++ cpp/HotelReservationSystem/main.cpp
@@ -7,11 +7,11 @@ using namespace std;
 class Guest {
     private:
         string name;
-        int roomNum;
+        string roomNum;
         string checkIn;
         string checkOut;
     public:
-        Guest(string name = "", int roomNum = 0, string checkIn = " ", string checkOut = " ") {
+        Guest(string name = "", string roomNum = " ", string checkIn = " ", string checkOut = " ") {
             this -> name = name;
             this -> roomNum = roomNum;
             this -> checkIn = checkIn;
@@ -19,19 +19,19 @@ class Guest {
         }
 //      GETTERS
         string getName() { return name; }
-        int getRoomNum() { return roomNum; }
+        string getRoomNum() { return roomNum; }
         string getCheckIn() { return checkIn; }
         string getCheckOut() { return checkOut; }
         string getGuest() {
             string message = name + "\n"; 
-            message += to_string(roomNum) + "\n";
+            message += roomNum + "\n";
             message += checkIn + "\n";
             message += checkOut + "\n";
             return message;
         }
 //      SETTERS
         void setName(string aName) { this  -> name = aName;}
-        void setRoomNum(int aRoomNum) { this -> roomNum = aRoomNum;}
+        void setRoomNum(string aRoomNum) { this -> roomNum = aRoomNum;}
         void setCheckIn(string aCheckIn) { this -> checkIn = aCheckIn;}
         void setCheckout(string aCheckOut) { this -> checkOut = aCheckOut;}
 
@@ -39,64 +39,83 @@ class Guest {
 
 class Room {
     private:
-        int roomID;
+        string roomID;
         bool isAvailable;
     public:
-        Room(int roomID = 0, bool isAvailable = false) {
+        Room(string roomID = " ", bool isAvailable = false) {
             this -> roomID = roomID;
             this -> isAvailable = isAvailable;
         }
 //      GETTERS
-        int getRoomID() { return roomID;}
+        string getRoomID() { return roomID;}
         bool getIsAvailable() { return isAvailable;}
         string getRoom() { 
             string message = "Room ";
-            message += to_string(roomID);
-            message += " is ";
+            message += roomID;
+            message += " || ";
             message += (isAvailable ? "Vacant" : "Occupied");
             return message;
         }
 //      SETTERS
-        void setRoomID(int aRoomID) { this -> roomID = aRoomID;}
+        void setRoomID(string aRoomID) { this -> roomID = aRoomID;}
         void setIsAvailable(bool aIsAvailable) { this -> isAvailable = aIsAvailable;}
 };
 
-int showMenu();
+void showMenu();
 void displayAllRooms(vector<Room> firstFloor);
+void makeReservation(string name, string roomNum, string checkIn, string checkOut, vector <Guest> &reservations, vector <Room> firstFloor);
 
 int main() {
 
+int choice;
+string name, checkIn, checkOut, roomNum, input;
+
+
 vector <Room> firstFloor{
-    Room( 101, true),
-    Room( 102, true),
-    Room( 103, true),
-    Room( 104, true),
-    Room( 105, false)
+    Room( "101", true),
+    Room( "102", true),
+    Room( "103", true),
+    Room( "104", true),
+    Room( "105", false)
 };
 
 vector <Guest> reservations  = {
-    Guest("Nabunturan", 101, "2025-10-10", "2025-10-11")
+    Guest("Nabunturan", "101", "2025-10-10", "2025-10-11")
 };
 
-int userChoice = showMenu();
+
+showMenu();
+    getline(cin, input);
+    choice = stoi(input);
+
+     displayAllRooms(firstFloor);
 
 do {
+showMenu();
+    getline(cin, input);
+    choice = stoi(input);
 
-switch (userChoice) {
-    userChoice;
+switch (choice) {
     case 1:
         displayAllRooms(firstFloor);
         break;
     case 2:
+        makeReservation(name, roomNum, checkIn, checkOut,reservations, firstFloor);
         break;
-
-int userChoice = showMenu();
+    case 3:
+        break;
+    case 4:
+        break;
+    case 5:
+        break;
+    case 6:
+        cout << "Exiting program...";
+        break;
+    default:
+        cout << "Invalid input";
 };
-
-
-
 }
-while (userChoice == 6);
+while (choice != 6);
 
 
 
@@ -111,8 +130,7 @@ while (userChoice == 6);
 
 
 // FUNCTIONSSSSS
-int showMenu() {
-    int choice;
+void showMenu() {
     cout << "\n===== HOTEL RESERVATION MENU =====" << endl;
     cout << "1. Display all rooms" << endl;
     cout << "2. Make a reservation" << endl;
@@ -121,38 +139,45 @@ int showMenu() {
     cout << "5. Cancel reservation" << endl;
     cout << "6. Exit" << endl;
     cout << "Enter choice: ";
-    cin >> choice;
-    return choice;
-    
-}
+}   
+
 
 void displayAllRooms(vector<Room> firstFloor) {
+    cout << "\n===== LIST OF ROOMS =====" << endl;
     for (int i = 0; i < firstFloor.size(); i++) {
     cout << firstFloor[i].getRoom() << endl;
 }
 }
+void makeReservation(string name, string roomNum, string checkIn, string checkOut, vector <Guest>& reservations, vector <Room> &firstFloor) {
+    cout << "Enter guest name: ";
+    getline(cin, name);
+
+    cout << "Enter room number: ";
+    getline(cin, roomNum);
+/*         for (int i = 0; i < size(firstFloor); i++){
+            if (roomNum == firstFloor.getRoomID()) {
+
+            }
+        } */
+
+    cout << "Enter check-in date: ";
+    getline(cin, checkIn);
+
+    cout << "Enter check-out date: ";
+    getline(cin, checkOut);
+
+    cout << "\n";
+
+    Guest newGuest(name, roomNum, checkIn, checkOut );
+
+    reservations.push_back(newGuest);
+
+    for (int i = 0; i < reservations.size(); i++) {
+        cout << reservations[i].getGuest() << endl;
+    }
+}
 
 
 
 
-/* string name, checkIn, checkOut;
-int roomNum;
 
-cout << "Enter guest name: ";
-getline(cin, name);
-
-cout << "Enter room number: ";
-cin >> roomNum;
-cin.ignore();
-
-cout << "Enter check-in date: ";
-getline(cin, checkIn);
-
-cout << "Enter check-out date: ";
-getline(cin, checkOut);
-
-Guest newGuest(name, roomNum, checkIn, checkOut);
-
-reservations.push_back(newGuest);
-
-cout <<reservations.back().getGuest(); */
